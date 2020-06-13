3c550abac9f9a992dbba51d299bf51b07c7d80dc
rinat <rrrinat84@gmail.com>
2020-06-13 05:21:20 +0300

    first commit

---
+++ Assets/_Content/Scripts/BezierPath.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BezierPath : MonoBehaviour
+{
+    [Range(0f, 1f)]
+    public float step;
+
+    private Path _path;
+
+    private void Start()
+    {
+        _path = FindObjectOfType<Path>();
+        _path.Initialize();
+    }
+
+    private void Update()
+    {
+        Test();
+    }
+
+    private void Test()
+    {
+        for (int i = 0; i < _path.segmentsCount; i++)
+        {
+            var points = _path.PointsInSegments(i);
+            DrawCurve(points);
+        }
+    }
+
+    private void DrawCurve(List<Vector3> points)
+    {
+        var drawPoints = new Queue<Vector3>();
+
+        for (float i = 0; i < 1; i = i + step)
+        {
+            var point = Evaluate(points[0], points[1], points[2], points[3], i);
+            drawPoints.Enqueue(point);
+        }
+
+        var lastPoint = drawPoints.Dequeue();
+        while (drawPoints.Count > 0)
+        {
+            var current = drawPoints.Dequeue();
+            Debug.DrawLine(lastPoint, current, Color.red);
+
+            lastPoint = current;
+        }
+    }
+
+    private Vector3 Evaluate(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4, float value)
+    {
+        var t = Mathf.Clamp01(value);
+        var difference = 1f - t;
+
+        return Mathf.Pow(difference, 3) * p1 + 3 * Mathf.Pow(difference, 2) * t * p2 +
+               3 * difference * t * t * p3 + t * t * t * p4;
+    }
+
+}
---
+++ Assets/_Content/Scripts/Path.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+    public float radius;
+    public List<Transform> points;
+
+    private List<Vector3> _points;
+
+    public int segmentsCount
+    {
+        get { return (_points.Count - 4) / 3 + 1; }
+    }
+
+    public List<Vector3> PointsInSegments(int i)
+    {
+        return new List<Vector3>()
+        {
+            _points[i * 3],
+            _points[i * 3 + 1],
+            _points[i * 3 + 2],
+            _points[i * 3 + 3]
+        };
+    }
+
+    public void Initialize()
+    {
+        _points = new List<Vector3>();
+        foreach (var point in points)
+        {
+            _points.Add(point.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (points.Count == 0)
+        {
+            return;
+        }
+
+        //spheres
+        Gizmos.color = Color.green;
+        foreach (var point in points)
+        {
+            Gizmos.DrawSphere(point.position, radius);
+        }
+        //lines
+        Gizmos.color = Color.white;
+        var lastPoint = points[0];
+        foreach (var point in points)
+        {
+            Gizmos.DrawLine(point.position, lastPoint.position);
+            lastPoint = point;
+        }
+    }
+
+}
